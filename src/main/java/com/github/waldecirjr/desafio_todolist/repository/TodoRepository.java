4891cc946eef0b58bc1b0f9cc06a4ebc44690e24
Waldecir Júnior <waldeciroajunior@hotmail.com>
2025-06-26 17:08:56 -0300

    Config controller, entity, repository e service

---
+++ src/main/java/com/github/waldecirjr/desafio_todolist/repository/TodoRepository.java
@@ -0,0 +1,8 @@
+package com.github.waldecirjr.desafio_todolist.repository;
+
+import com.github.waldecirjr.desafio_todolist.entity.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository <Todo, Long>{
+
+}
